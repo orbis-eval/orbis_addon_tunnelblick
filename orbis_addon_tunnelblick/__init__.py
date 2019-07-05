74267ca90510a653d26a8deb0e81b43e46cfc572
fabod <2189668+fabod@users.noreply.github.com>
2019-07-05 14:35:35 +0200

    wip

--- orbis_addon_tunnelblick/__init__.py
+++ orbis_addon_tunnelblick/__init__.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_addon_tunnelblick"
+__version__ = "1.1.dev"
+__author__ = "fabod"
+__year__ = "2019"
+__description__ = "The Tunnelblick Addon for Orbis view gold standards."
+__license__ = "GPL2"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_addon_tunnelblick"
+__type__ = "addon"
+
 from .main import Tunnelblick as Main
